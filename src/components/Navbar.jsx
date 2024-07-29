99c77b769e0fc07e0d1f60c7336628bc527c90ec
Nadim Anwar <anwarnadim3@gmail.com>
2024-07-29 17:30:14 +0530

    FIrst Commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function Navbar() {
     return (
-        <div className='bg-red-300'>Navbar</div>
+        <div className='bg-slate-700 h-20 w-full p-4 flex items-center justify-center'>
+            <h2 className='text-white text-2xl font-bold'>Calculator.io</h2>
+        </div>
     )
 }
 
